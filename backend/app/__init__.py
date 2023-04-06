a0a175bcd7741104fa008ae5e317c3755ddfdcdb
Daniel Briscoe <extradrivespace123456@gmail.com>
2023-04-06 12:01:41 -0600

    first compiling version

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,2 @@
-from flask import request, jsonify
-from backend.app import app
-
-@app.route("/api/my_function", methods=["POST"])
-def my_function():
-    # Process the request and call your Python function
-    # Return a JSON response
-    return jsonify({"result": "success"})
+from .app import app
+from . import routes
